b5d62823bd04096a24a548ae2cfee0b8ca7fd07a
Oumayma Razik <e41877u@etu.univ-lorraine.fr>
2025-09-19 09:28:05 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# software\_engineering\_project\_2025
+
+This is the first soft engineering project built using cmake - ENSG 2025
+
+Professor : François BONNEAU
+
+
+
---
+++ mains/hello.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <HelloProjConfig.h>
+
+void hello_world() 
+{
+std::cout << "Hello World" << std::endl;
+}
+
+void print_version() {
+	std::cout << "project version is"
+		<< HelloProj_VERSION_MAJOR
+		<< ". "
+		<< HelloProj_VERSION_MINOR
+		<< std::endl;
+}
+
+int main()
+{
+hello_world();
+print_version();
+}
