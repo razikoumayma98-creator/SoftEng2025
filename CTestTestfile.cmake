efef740e9fba554f554b78ee0f5c60b3f6d940a4
Oumayma Razik <e41877u@etu.univ-lorraine.fr>
2025-09-22 10:20:20 +0200

    delete unecessary files

--- CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/e41877u/Documents/Software_engineering/software_engineering_project_2025
-# Build directory: C:/Users/e41877u/Documents/Software_engineering/software_engineering_project_2025
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src/basic")
-subdirs("mains")
-subdirs("tests")
